3c3bc4b47a4e64dec0de486383b476bb541c300b
Kolakanmi Apanisile <33889523+Kolakanmi@users.noreply.github.com>
2020-11-17 12:07:04 +0000

    css

--- src/components/SingleProduct.vue
+++ src/components/SingleProduct.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <div>
-            <img :src=image_url>
-            <span>{{title}}</span>
-            <span>{{price}}</span>
-            <button @click="() => addToCart({title, id, price, image_url, quantity:1})">Add to cart</button>
+        <div class="productbox">
+            <img :src=image_url class="">
+            <div class="productbox_title">{{title}}</div>
+            <div class="productbox_price">$ {{price}}</div>
+            <button class="productbox_btn" @click="() => addToCart({title, id, price, image_url, quantity:1})">Add to cart</button>
         </div>
     </div>
 </template>
@@ -21,6 +21,35 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .productbox{
+        width: 30.3%;
+        height: 350px;
+        float: left;
+        margin: 12px 1.5%;
 
+        img{
+            width: 70%;
+            object-fit: contain;
+            height: 200px;
+            margin: 0 auto 30px auto;
+            display: table;
+        }
+
+        &_title{
+            text-align: center;
+        }
+        &_price{
+            text-align: center;
+            margin: 20px auto;
+        }
+        &_btn{
+            padding: 10px 25px;
+            background: green;
+            border: none;
+            margin: 0 auto;
+            display: table;
+            color: #ffffff;
+        }
+    }
 </style>
